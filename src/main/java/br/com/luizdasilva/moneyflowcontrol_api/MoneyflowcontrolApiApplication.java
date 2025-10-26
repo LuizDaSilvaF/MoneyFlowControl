08e7f58834a218f5d83079d5e885687a3c132041
LuizDaSilvaF <jobluizdasilvaofc@gmail.com>
2025-10-26 10:37:37 -0300

    config(database): Configures database connection and flyway migration

---
+++ src/main/java/br/com/luizdasilva/moneyflowcontrol_api/MoneyflowcontrolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizdasilva.moneyflowcontrol_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyflowcontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyflowcontrolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizdasilva/moneyflowcontrol_api/MoneyflowcontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizdasilva.moneyflowcontrol_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyflowcontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
