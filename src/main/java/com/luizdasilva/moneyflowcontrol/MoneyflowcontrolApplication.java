748c911b6191401f847066f30b2dac813d029b8b
LuizDaSilvaF <jobluizdasilvaofc@gmail.com>
2026-06-25 13:07:16 -0300

    folder structure

---
+++ src/main/java/com/luizdasilva/moneyflowcontrol/MoneyflowcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.luizdasilva.moneyflowcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyflowcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyflowcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizdasilva/moneyflowcontrol/MoneyflowcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdasilva.moneyflowcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyflowcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
